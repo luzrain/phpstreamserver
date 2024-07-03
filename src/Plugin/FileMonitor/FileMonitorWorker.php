d6210ed819bba4d7c99204416647041030c0da19
Anton <anton.z@live.com>
2024-07-04 00:08:29 +0800

    Added FileMonitor plugin

---
+++ src/Plugin/FileMonitor/FileMonitorWorker.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Luzrain\PHPStreamServer\Plugin\FileMonitor;
+
+use Luzrain\PHPStreamServer\Plugin\FileMonitor\FileMonitorWatcher\FileMonitorWatcher;
+use Luzrain\PHPStreamServer\WorkerProcess;
+
+final class FileMonitorWorker extends WorkerProcess
+{
+    public function __construct(
+        private array $sourceDir,
+        private array $filePattern,
+        private float $pollingInterval,
+        string|null $user,
+        string|null $group,
+        private \Closure $reloadCallback,
+    ) {
+        parent::__construct(
+            name: 'File monitor',
+            user: $user,
+            group: $group,
+            reloadable: false,
+            onStart: $this->onStart(...),
+        );
+    }
+
+    private function onStart(): void
+    {
+        $fileMonitor = FileMonitorWatcher::create(
+            $this->getLogger(),
+            $this->sourceDir,
+            $this->filePattern,
+            $this->pollingInterval,
+            $this->doReload(...),
+        );
+        $fileMonitor->start($this->getEventLoop());
+    }
+
+    /**
+     * @psalm-suppress NoValue
+     * @psalm-suppress RiskyTruthyFalsyComparison
+     */
+    private function doReload(): void
+    {
+        ($this->reloadCallback)();
+
+        if (\function_exists('opcache_get_status') && $status = \opcache_get_status()) {
+            foreach (\array_keys($status['scripts'] ?? []) as $file) {
+                \opcache_invalidate($file, true);
+            }
+        }
+    }
+}
