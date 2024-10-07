054a7ca83dd9c94bba564c62992bad50f62a22e5
Anton <anton.z@live.com>
2024-10-07 23:46:42 +0800

    SimpleLogger implementation now can colorize output and works asynchronously.

--- src/Internal/Logger/SimpleLogger.php
+++ src/Internal/Logger/SimpleLogger.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Luzrain\PHPStreamServer\Internal\Logger;
 
+use Amp\ByteStream\WritableResourceStream;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LoggerTrait;
+use function Amp\async;
 
 /**
  * @internal
@@ -16,20 +18,34 @@ final readonly class SimpleLogger implements LoggerInterface
 
     private const DEFAULT_JSON_FLAGS = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_INVALID_UTF8_SUBSTITUTE | JSON_PARTIAL_OUTPUT_ON_ERROR;
 
-    /**
-     * @var resource
-     */
-    private mixed $stream;
+    private const LEVELS_COLOR_MAP = [
+        'debug' => '<color;fg=15>DEBUG</>',
+        'info' => '<color;fg=116>INFO</>',
+        'notice' => '<color;fg=38>NOTICE</>',
+        'warning' => '<color;fg=yellow>WARNING</>',
+        'error' => '<color;fg=red>ERROR</>',
+        'critical' => '<color;fg=red>CRITICAL</>',
+        'alert' => '<color;fg=red>ALERT</>',
+        'emergency' => '<color;bg=red>EMERGENCY</>',
+    ];
 
-    public function __construct()
+    private WritableResourceStream $stream;
+
+    /**
+     * @var $resource $resource
+     */
+    public function __construct($resource = STDERR)
     {
-        $this->stream = STDERR;
+        $this->stream = new WritableResourceStream($resource);
     }
 
     public function log(mixed $level, string|\Stringable $message, array $context = []): void
     {
-        $message = $this->format((string) $level, (string) $message, $context);
-        \fwrite($this->stream, $message . PHP_EOL);
+        $formattedMessage = $this->format((string) $level, (string) $message, $context);
+
+        async(function () use ($formattedMessage) {
+            $this->stream->write($formattedMessage. PHP_EOL);
+        });
     }
 
     private function format(string $level, string $message, array $context): string
@@ -44,9 +60,10 @@ final readonly class SimpleLogger implements LoggerInterface
             $message = \strtr($message, $replacements);
         }
 
-        $formattedMessage = \sprintf('%s [%s] %s', \date(\DateTimeInterface::RFC3339), $level, $message);
-        $formattedContext = $context !== [] ? ' ' . \json_encode($context, self::DEFAULT_JSON_FLAGS) : '';
+        $date = \date('Y-m-d H:i:s');
+        $level = self::LEVELS_COLOR_MAP[$level] ?? $level;
+        $context = $context !== [] ? '' . \json_encode($context, self::DEFAULT_JSON_FLAGS) : '';
 
-        return $formattedMessage . $formattedContext;
+        return \rtrim(\sprintf("%s  %s\t<color;fg=green>%s</>\t%s %s", $date, $level, 'phpss', $message, $context));
     }
 }
