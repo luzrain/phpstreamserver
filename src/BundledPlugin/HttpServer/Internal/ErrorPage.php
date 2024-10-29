92f8dd0552a56d87d13baf441d91c7c821021246
Anton <anton.z@live.com>
2024-10-30 02:13:42 +0800

    Error page now shows previous exceptions

--- src/BundledPlugin/HttpServer/Internal/ErrorPage.php
+++ src/BundledPlugin/HttpServer/Internal/ErrorPage.php
@@ -48,6 +48,20 @@ final readonly class ErrorPage
     private function getTemplateWithException(\Throwable $exception): string
     {
         $exceptionClass = $exception::class;
+
+        $previousHtml = '';
+        if (null !== $previous = $exception->getPrevious()) {
+            do {
+                $previousClass = $previous::class;
+                $previousHtml .= <<<HTML
+                    <div style="font-size:0.90em;">
+                        <div>Previous {$previousClass}({$previous->getCode()}): "{$previous->getMessage()}"</div>
+                        <div style="font-size:0.85em;">in <b>{$previous->getFile()}</b> on line <b>{$previous->getLine()}</b></div>
+                    </div>
+                HTML;
+            } while (null !== $previous = $previous->getPrevious());
+        }
+
         return <<<HTML
             <!DOCTYPE html>
             <html lang="en">
@@ -56,15 +70,18 @@ final readonly class ErrorPage
                 <style>
                     body {color: #333; font-family: Verdana, sans-serif; font-size: 0.9em; margin: 0; padding: 1rem;}
                     h1 {margin: 0;}
-                    pre {margin: 1rem 0; white-space: pre-wrap;}
+                    pre {margin: 1rem 0; white-space: pre; overflow-y: auto; font-size:0.95em;}
                     hr {margin: 1rem 0; border: 0; border-bottom: 1px solid #333;}
                 </style>
             </head>
             <body>
             <h1>{$this->status} {$this->reason}</h1>
-            <div style="margin: 1rem 0;">
-                <div>{$exceptionClass}: {$exception->getMessage()}</div>
-                <div style="font-size:0.85em;">in <b>{$exception->getFile()}</b> on line <b>{$exception->getLine()}</b></div>
+            <div style="margin: 1rem 0; display: flex; flex-direction: column; gap: 0.33rem;">
+                <div>
+                    <div>Exception {$exceptionClass}({$exception->getCode()}): "{$exception->getMessage()}"</div>
+                    <div style="font-size:0.85em;">in <b>{$exception->getFile()}</b> on line <b>{$exception->getLine()}</b></div>
+                </div>
+                {$previousHtml}
             </div>
             <pre>Stack trace:\n{$exception->getTraceAsString()}</pre>
             <hr>
