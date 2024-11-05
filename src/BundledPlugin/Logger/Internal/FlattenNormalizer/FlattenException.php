d4e254d3dfa4b468d087f6e5103e82d4ad61b25a
Anton <anton.z@live.com>
2024-11-06 03:08:19 +0800

    FlattenException commants

--- src/BundledPlugin/Logger/Internal/FlattenNormalizer/FlattenException.php
+++ src/BundledPlugin/Logger/Internal/FlattenNormalizer/FlattenException.php
@@ -37,6 +37,29 @@ final readonly class FlattenException implements \Stringable
         );
     }
 
+    /**
+     * Gets a multiline string representation of the thrown object with a stack race, same as the original exception
+     */
+    public function __toString(): string
+    {
+        $out = \sprintf("%s: %s in %s:%d\nStack trace:\n", self::parseAnonymousClass($this->class), $this->message, $this->file, $this->line);
+        $i = 0;
+        foreach ($this->trace as $trace) {
+            $file = $trace['file'] ?? null;
+            $line = $trace['line'] ?? null;
+            $class = $trace['class'] ?? null;
+            $type = $trace['type'] ?? null;
+            $function = $trace['function'] ?? '[unknown function]';
+            $fileStr = $file !== null && $line !== null ? \sprintf('%s(%d)', $file, $line) : '[internal function]';
+            $functionStr = $class !== null && $type !== null ? self::parseAnonymousClass($class) . $type . $function : $function;
+            $out .= \sprintf("#%d %s: %s()\n", $i++, $fileStr, $functionStr);
+        }
+        return $out . \sprintf("#%d {main}", $i);
+    }
+
+    /**
+     * Gets a short string representation suitable for logging
+     */
     public function toString(): string
     {
         $str = $this->formatException('exception');
@@ -68,22 +91,5 @@ final readonly class FlattenException implements \Stringable
             ? (\get_parent_class($class) ?: \key(\class_implements($class)) ?: 'class').'@anonymous'
             : $class
         ;
-    }
-
-    public function __toString(): string
-    {
-        $out = \sprintf("%s: %s in %s:%d\nStack trace:\n", self::parseAnonymousClass($this->class), $this->message, $this->file, $this->line);
-        $i = 0;
-        foreach ($this->trace as $trace) {
-            $file = $trace['file'] ?? null;
-            $line = $trace['line'] ?? null;
-            $class = $trace['class'] ?? null;
-            $type = $trace['type'] ?? null;
-            $function = $trace['function'] ?? '[unknown function]';
-            $fileStr = $file !== null && $line !== null ? \sprintf('%s(%d)', $file, $line) : '[internal function]';
-            $functionStr = $class !== null && $type !== null ? self::parseAnonymousClass($class) . $type . $function : $function;
-            $out .= \sprintf("#%d %s: %s()\n", $i++, $fileStr, $functionStr);
-        }
-        return $out . \sprintf("#%d {main}", $i);
     }
 }
